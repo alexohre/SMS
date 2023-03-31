413cf379b698242da81919ae1f655f0149811ae5
alexohre <alexokomado@gmail.com>
2023-03-31 09:09:50 +0100

    fixed background image
    Signed-off-by: alexohre <alexokomado@gmail.com>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,5 @@
 import "@hotwired/turbo-rails";
 import "./controllers";
 
+import "./custom/bootstrap";
 import "./custom/scripts";
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  get 'pages/contact'
+  get 'contact', to: 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
