591ed07a23a1650d9769e0ffcd7de111c9653987
alexohre <alexokomado@gmail.com>
2023-04-01 14:19:25 +0100

    integrated sb-admin-2 for admin dashboard
    Signed-off-by: alexohre <alexokomado@gmail.com>

--- app/javascript/admin.js
+++ app/javascript/admin.js
@@ -1,2 +1,11 @@
 import "@hotwired/turbo-rails";
 import "./admin_controllers";
+
+import "./admin_custom/jq";
+import "./admin_custom/bootstrap.bundle";
+import "./admin_custom/jquery.easing";
+import "./admin_custom/admin";
+import "./admin_custom/Chart";
+import "./admin_custom/chart-area-demo";
+import "./admin_custom/chart-pie-demo";
+// import "";
