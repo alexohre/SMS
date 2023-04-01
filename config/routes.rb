9f436a84a9f7d082ddaca98e37fbe76f67efac78
alexohre <alexokomado@gmail.com>
2023-04-01 13:13:27 +0100

    seperated admin from public view
    Signed-off-by: alexohre <alexokomado@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'dashboard', to: 'dashboard#index'
+  end
   root 'pages#index'
   get 'contact', to: 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
