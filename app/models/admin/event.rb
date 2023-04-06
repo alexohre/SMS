32f17b3b82f0a0f11c472622b9663aeac5399668
alexohre <alexokomado@gmail.com>
2023-04-06 11:38:15 +0100

    completed events
    Signed-off-by: alexohre <alexokomado@gmail.com>

--- app/models/admin/event.rb
+++ app/models/admin/event.rb
@@ -3,9 +3,10 @@ class Admin::Event < ApplicationRecord
   belongs_to :admin_category, class_name: "Admin::Category"
 
   has_one_attached :photo
-  validates :name, presence: true, uniqueness: true, length: {maximum: 50}
+  validates :name, presence: true, uniqueness: true, length: {maximum: 60}
   validates :description, presence: true
   validates :photo, presence: true
+  # validates :photo, presence: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { less_than: 5.megabytes }
   validates :start_date, presence: true
   validates :start_time, presence: true
 
