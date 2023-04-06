b317c73df78e9bd9f3f173dcd52765d437ce526a
alexohre <alexokomado@gmail.com>
2023-04-06 10:38:12 +0100

    fixed category delete error
    Signed-off-by: alexohre <alexokomado@gmail.com>

--- app/models/admin/category.rb
+++ app/models/admin/category.rb
@@ -1,5 +1,5 @@
 class Admin::Category < ApplicationRecord
-  has_many :admin_events, dependent: :destroy
+  has_many :admin_events, class_name: "Admin::Event", foreign_key: "admin_category_id", dependent: :destroy
   validates :name, presence: true, uniqueness: true
   
 end
